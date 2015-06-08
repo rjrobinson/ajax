8836876c68be5eb5000ed4081c1c801087e24a68
RJ Robinson <robertj.robinson@gmail.com>
2015-06-08 16:18:17 -0400

    pulled out functions and created Ttutsplus AJAX lib

---
+++ public/Tutsplus.js
@@ -0,0 +1,35 @@
+var Tutsplus = {}
+
+Tutsplus.createXHR = function(url, options) {
+    var xhr = false;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    }
+
+    if (xhr) {
+        options = options || {};
+        options.method = options.method || "GET"
+
+        xhr.onreadystatechange = function() {
+            if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
+                if (options.complete) {
+                    options.complete.call(xhr, JSON.parse(xhr.responseText));
+                }
+            }
+        };
+
+        xhr.open(options.method, url, true);
+        return xhr
+
+    } else {
+        return false
+    }
+
+}
+Tutsplus.ajax = function(url, options) {
+    var xhr = Tutsplus.createXHR(url, options);
+
+    if (xhr) {
+        xhr.send(null);
+    }
+};
--- public/main.js
+++ public/main.js
@@ -3,24 +3,14 @@
     var link = document.getElementsByTagName("a")[0];
 
     link.onclick = function() {
-        // XHR OBJ
-        var xhr = new XMLHttpRequest();
 
-        // handle the onReady state change event
-        // xhr.readyState property values
-        //  0 = Uninitalized
-        //  1 = Loading
-        //  2 = Loaded
-        //  3 = Interactive
-        //  4 = Complete
+        Tutsplus.ajax('ajax.json', {
+            method: "GET",
+            complete: function(response) {
 
-        xhr.onreadystatechange = function() {
-            console.log(xhr.status);
-            if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
-                console.log(xhr.status);
                 var body = document.getElementsByTagName("body")[0];
 
-                var json = JSON.parse(xhr.responseText);
+                var json = response;
 
                 var heading = json.heading;
                 var h2 = document.createElement("h2")
@@ -47,13 +37,9 @@
 
                 body.removeChild(link);
             }
-        };
 
-        // Open request.
-        xhr.open("GET", "ajax.json", true);
+        });
 
-        // send request to server.
-        xhr.send(null);
-        return false;
-    };
+        return false
+    }
 })();
